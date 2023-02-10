381268aab9028b0f0353653034df53bfee86c60a
Marc Wieland <inf3457@hs-worms.de>
2023-02-10 11:45:58 +0100

    MW Branch creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+//Hier mal ein Kommi, um den Branch zu testen
 
 void main() {
   runApp(const MyApp());
