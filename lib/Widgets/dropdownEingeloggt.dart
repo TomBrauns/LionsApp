78e15f10a6b50f7b8d6c584884e4f3de6b98345e
Kristiyan Ivanov <inf3563@hs-worms.de>
2023-02-14 16:16:59 +0100

    dropdown widget added

---
+++ lib/Widgets/dropdownEingeloggt.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+const List<String> list = <String>[
+  'keins',
+  'pro Monat',
+  'alle 6 Monate',
+  'jedes Jahr'
+];
+
+class DropdownButtonList extends StatelessWidget {
+  const DropdownButtonList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('DropdownButton Sample')),
+        body: const Center(
+          child: DropdownButtonExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      style: const TextStyle(color: Colors.deepPurple),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
