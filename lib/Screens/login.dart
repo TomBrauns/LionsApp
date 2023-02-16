033bcd4fbba6442e3399aad409d67007cc6d5d1b
Kristiyan Ivanov <inf3563@hs-worms.de>
2023-02-16 11:38:17 +0100

    login screen changed a lot

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,35 +1,82 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
-
+class LoginScreen extends StatefulWidget {
   @override
-  State<Login> createState() => _LoginState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late String _nameOrEmail;
+  late String _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(children: [
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Name/Email',
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Name/Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Bitte geben Sie Ihren Namen oder Ihre E-Mail-Adresse ein!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _nameOrEmail = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Passwort',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Bitte geben Sie Ihr Passwort ein!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        // TODO: Add login functionality
+                      }
+                    },
+                    child: Text('Anmelden'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // TODO: Handle password forgotten functionality
+                    },
+                    child: Text('Passwort vergessen?'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Passwort',
-          ),
-        ),
-        ElevatedButton(onPressed: null, child: Text("Anmelden")),
-        ElevatedButton(onPressed: null, child: Text("Passwort vergessen?")),
-      ]),
-    ));
+      ),
+    );
   }
 }
