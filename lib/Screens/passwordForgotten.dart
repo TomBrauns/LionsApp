e45db6e710f3f1a8415d406b708e29e7ab74334f
Kristiyan Ivanov <inf3563@hs-worms.de>
2023-02-16 11:53:44 +0100

    password forgotten changed a lot

--- lib/Screens/passwordForgotten.dart
+++ lib/Screens/passwordForgotten.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 
-class PasswordForgotten extends StatefulWidget {
-  const PasswordForgotten({Key? key}) : super(key: key);
-
+class PasswordForgottenScreen extends StatefulWidget {
   @override
-  State<PasswordForgotten> createState() => _PasswordForgottenState();
+  _PasswordForgottenScreenState createState() =>
+      _PasswordForgottenScreenState();
 }
 
-class _PasswordForgottenState extends State<PasswordForgotten> {
+class _PasswordForgottenScreenState extends State<PasswordForgottenScreen> {
+  final TextEditingController _emailController = TextEditingController();
+
+  void _submitForm() {
+    // TODO: Implement functionality to send email to reset password
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(children: [
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Email',
-          ),
+      appBar: AppBar(
+        title: Text('Password vergessen'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Bitte geben Sie Ihre E-Mail-Adresse ein, um Ihr Passwort zurückzusetzen:',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'E-Mail',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submitForm,
+              child: Text('Bestätigen'),
+            ),
+          ],
         ),
-        ElevatedButton(onPressed: null, child: Text("Bestätigen")),
-      ]),
-    ));
+      ),
+    );
   }
 }
