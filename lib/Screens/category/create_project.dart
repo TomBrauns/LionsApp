caa5034876548f1e6abd91d8efdf29ab64454842
Marc Wieland <inf3457@hs-worms.de>
2023-02-16 11:41:54 +0100

    Event Create eingestellt

---
+++ lib/Screens/category/create_project.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lionsapp/Widgets/burgermenu.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
+
+  @override
+  State<Chat> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const BurgerMenu(),
+      appBar: AppBar(
+        title: const Text("Projekt anlegen"),
+      ),
+      body:
+
+    );
+  }
+}
