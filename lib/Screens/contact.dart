3f70d5afa2038a2d9256ab6647c32aae57a07ef0
Omer Guimdo <inf3249@hs-worms.de>
2023-02-16 13:28:20 +0100

    contact form with file upload

--- lib/Screens/contact.dart
+++ lib/Screens/contact.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 import 'package:lionsapp/Widgets/burgermenu.dart';
 import 'package:mb_contact_form/mb_contact_form.dart';
@@ -11,6 +13,8 @@ class Contact extends StatefulWidget {
 
 class _ContactState extends State<Contact> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  get child => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +22,29 @@ class _ContactState extends State<Contact> {
       appBar: AppBar(
         title: const Text("Kontakt"),
       ),
-      body: const MBContactForm(
-          hasHeading: true,
-          withIcons: false,
-<<<<<<< HEAD
-          destinationEmail: "omersucces@yahoo.com"),
-=======
-          destinationEmail: "XXXXX@yahoo.com"),
->>>>>>> 48ce195fa88f4584b16c4bbc9354a889683ed62d
+      body: const SingleChildScrollView(
+        padding: EdgeInsetsDirectional.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            MBContactForm(
+                hasHeading: true,
+                withIcons: false,
+                destinationEmail: "XXXXX@yahoo.com"),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+              onPressed: null,
+              child: Text(
+                "Weitere Datei zu hochladen hier reinziehen",
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  color(Color white) {}
 }
