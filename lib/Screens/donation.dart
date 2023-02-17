3df377bb0c0bc465c5770e65a0fadc16e8e9de4c
Tom Brauns <inf3621@hs-worms.de>
2023-02-17 15:52:29 +0100

    (donation.dart might be fucked)

--- lib/Screens/donation.dart
+++ lib/Screens/donation.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:lionsapp/Screens/user_configs.dart';
+import 'package:lionsapp/Screens/user/user_configs.dart';
 import 'package:lionsapp/Widgets/burgermenu.dart';
 import 'package:lionsapp/Widgets/bottomNavigationView.dart';
 import 'package:lionsapp/Widgets/appbar.dart';
