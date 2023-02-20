941946f030302f605e25dc5dabe5f22c816694a3
Marc Wieland <inf3457@hs-worms.de>
2023-02-20 18:14:12 +0100

    Suchfunktion für die eventliste eingebaut

--- lib/Screens/events/events_liste.dart
+++ lib/Screens/events/events_liste.dart
@@ -30,12 +30,15 @@ class _EventsState extends State<Events> {
     );
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(title: "Events"),
       drawer: const BurgerMenu(),
       body: EventList(),
+
+
 
       //Alter ListView von Nico
 
@@ -74,60 +77,98 @@ class _EventsState extends State<Events> {
   }
 }
 
-class EventList extends StatelessWidget {
+class EventList extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('events').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        }
+  State<EventList> createState() => _EventListState();
+}
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+class _EventListState extends State<EventList>{
 
-        return ListView.builder(
-          itemCount: snapshot.data!.size,
-          itemBuilder: (BuildContext context, int index) {
-            final event = snapshot.data!.docs[index];
-            final String eventId = event.id;
+  late Stream<QuerySnapshot> _eventsStream;
+  late String _searchQuery;
 
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EventDetailsPage(eventId: eventId),
-                  ),
+  @override
+  void initState() {
+    super.initState();
+    _searchQuery = '';
+    _eventsStream = FirebaseFirestore.instance.collection('events').snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value){
+              setState(() {
+                _searchQuery = value;
+              });
+            },
+            decoration: const InputDecoration(
+              hintText: 'Event suchen',
+            ),
+          ),
+        ),
+        Expanded(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: _eventsStream,
+            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+              if(snapshot.hasError){
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
                 );
-              },
-              child: Card(
-                child: ListTile(
-                  title: Text(event['eventName']),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(event['eventInfo']),
-                      const SizedBox(height: 4),
-                      Row(children: [
-                        const Icon(Icons.location_on, size: 16),
-                        Text(event['ort']),
-                      ]),
-                      const SizedBox(height: 4),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        );
-      },
+              }
+
+              if(snapshot.connectionState == ConnectionState.waiting){
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final filteredEvents = snapshot.data!.docs.where((event) => event['eventName'].toLowerCase().contains(_searchQuery.toLowerCase()));
+
+              return ListView.builder(
+                itemCount: filteredEvents.length,
+                itemBuilder: (BuildContext context, int index){
+                  final event = filteredEvents.elementAt(index);
+                  final String eventId = event.id;
+
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EventDetailsPage(eventId: eventId),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      child: ListTile(
+                        title: Text(event['eventName']),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(event['eventInfo']),
+                            const SizedBox(height: 4),
+                            Row(children: [
+                              const Icon(Icons.location_on, size: 16),
+                              Text(event['ort']),
+                            ]),
+                            const SizedBox(height: 4),
+                          ],
+                        ),
+                      )
+                    )
+                  );
+                },
+              );
+            }
+          )
+        )
+      ],
     );
   }
 }
+
