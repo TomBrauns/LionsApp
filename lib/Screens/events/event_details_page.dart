c5b47d68669eadcd902b15fef59c1716bf15f496
Nico Hofmann <inf3207@hs-worms.de>
2023-02-23 15:50:27 +0100

    Fix: Use StreamBuilder for event details page

--- lib/Screens/events/event_details_page.dart
+++ lib/Screens/events/event_details_page.dart
@@ -16,39 +16,6 @@ class EventDetailsPage extends StatefulWidget {
 class _EventDetailsPageState extends State<EventDetailsPage> {
   final dateFormat = DateFormat("dd.MM.yyyy HH:mm");
 
-  late String _title = "";
-  late String _location = "";
-  late String _date = "";
-  late String _project = "";
-  late String _target = "";
-  late String _description = "";
-  late String _imgUri = "";
-
-  @override
-  void initState() {
-    _loadEventData();
-    super.initState();
-  }
-
-  void _loadEventData() async {
-    final docSnapshot = await FirebaseFirestore.instance.collection('events').doc(widget.eventId).get();
-
-    if (docSnapshot.exists) {
-      final data = docSnapshot.data()!;
-      setState(() {
-        _title = data['eventName'];
-        _location = data['ort'] ?? "";
-        if (data["startDate"] != null) {
-          _date = dateFormat.format((data['startDate'] as Timestamp).toDate());
-        }
-        _project = data['projekt'] ?? "";
-        _target = data['spendenZiel'] ?? "";
-        _imgUri = data['image_url'] ?? "";
-        _description = data['eventInfo'];
-      });
-    }
-  }
-
   // Style
   final TextStyle _headlineStyle = const TextStyle(fontWeight: FontWeight.bold, fontSize: 18);
   final TextStyle _textStyle = const TextStyle(fontSize: 16);
@@ -59,94 +26,122 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(_title),
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.edit),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => EventEditor(documentId: widget.eventId)));
-            }),
-        body: SingleChildScrollView(
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          if (_imgUri.isNotEmpty) Image.network(_imgUri, width: double.infinity, height: 250, fit: BoxFit.cover),
-          Container(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(children: [
-                    Expanded(
-                        child: Card(
-                            child: ListTile(
-                      title: Row(children: const [
-                        Icon(
-                          Icons.calendar_month,
-                          size: 16,
-                          color: Colors.black,
-                        ),
-                        Text("Datum")
-                      ]),
-                      subtitle: Text(_date, maxLines: 1),
-                    ))),
-                    Expanded(
-                        child: Card(
-                            child: ListTile(
-                      title: Row(children: const [
-                        Icon(
-                          Icons.location_on,
-                          size: 16,
-                          color: Colors.black,
-                        ),
-                        Text("Ort")
-                      ]),
-                      subtitle: Text(_location, maxLines: 1),
-                    ))),
-                  ]),
-                  Row(children: [
-                    Expanded(
-                        child: Card(
-                            child: ListTile(
-                      title: Row(children: const [
-                        Icon(
-                          Icons.supervised_user_circle,
-                          size: 16,
-                          color: Colors.black,
-                        ),
-                        Text("Zweck")
-                      ]),
-                      subtitle: Text(_project, maxLines: 1),
-                    ))),
-                    Expanded(
-                        child: Card(
-                            child: ListTile(
-                      title: Row(children: const [
-                        Icon(
-                          Icons.crisis_alert,
-                          size: 16,
-                          color: Colors.black,
-                        ),
-                        Text("Ziel")
-                      ]),
-                      subtitle: Text(_target, maxLines: 1),
-                    ))),
-                  ]),
-                  const SizedBox(height: 16),
-                  Text("Was machen wir?", style: _headlineStyle),
-                  const SizedBox(height: 4),
-                  Text(_description, style: _textStyle),
-                  const SizedBox(height: 32),
-                  Center(
-                      child: ElevatedButton(
-                    onPressed: _handleDonation,
-                    child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 4.0),
-                        child: Text("Spenden", style: TextStyle(fontSize: 22))),
-                  )),
-                  const SizedBox(height: 32),
-                ],
-              ))
-        ])));
+    return StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance.collection('events').doc(widget.eventId).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Fehler beim Lesen der Daten');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+
+          final String date;
+          final String title = data['eventName'];
+          final String location = data['ort'] ?? "";
+          if (data["startDate"] != null) {
+            date = dateFormat.format((data['startDate'] as Timestamp).toDate());
+          } else {
+            date = "";
+          }
+          final String project = data['projekt'] ?? "";
+          final String target = data['spendenZiel'] ?? "";
+          final String imgUri = data['image_url'] ?? "";
+          final String description = data['eventInfo'];
+
+          return Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+              ),
+              floatingActionButton: FloatingActionButton(
+                  child: const Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => EventEditor(documentId: widget.eventId)));
+                  }),
+              body: SingleChildScrollView(
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                if (imgUri.isNotEmpty) Image.network(imgUri, width: double.infinity, height: 250, fit: BoxFit.cover),
+                Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(children: [
+                          Expanded(
+                              child: Card(
+                                  child: ListTile(
+                            title: Row(children: const [
+                              Icon(
+                                Icons.calendar_month,
+                                size: 16,
+                                color: Colors.black,
+                              ),
+                              Text("Datum")
+                            ]),
+                            subtitle: Text(date, maxLines: 1),
+                          ))),
+                          Expanded(
+                              child: Card(
+                                  child: ListTile(
+                            title: Row(children: const [
+                              Icon(
+                                Icons.location_on,
+                                size: 16,
+                                color: Colors.black,
+                              ),
+                              Text("Ort")
+                            ]),
+                            subtitle: Text(location, maxLines: 1),
+                          ))),
+                        ]),
+                        Row(children: [
+                          Expanded(
+                              child: Card(
+                                  child: ListTile(
+                            title: Row(children: const [
+                              Icon(
+                                Icons.supervised_user_circle,
+                                size: 16,
+                                color: Colors.black,
+                              ),
+                              Text("Zweck")
+                            ]),
+                            subtitle: Text(project, maxLines: 1),
+                          ))),
+                          Expanded(
+                              child: Card(
+                                  child: ListTile(
+                            title: Row(children: const [
+                              Icon(
+                                Icons.crisis_alert,
+                                size: 16,
+                                color: Colors.black,
+                              ),
+                              Text("Ziel")
+                            ]),
+                            subtitle: Text(target, maxLines: 1),
+                          ))),
+                        ]),
+                        const SizedBox(height: 16),
+                        Text("Was machen wir?", style: _headlineStyle),
+                        const SizedBox(height: 4),
+                        Text(description, style: _textStyle),
+                        const SizedBox(height: 32),
+                        Center(
+                            child: ElevatedButton(
+                          onPressed: _handleDonation,
+                          child: const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 4.0),
+                              child: Text("Spenden", style: TextStyle(fontSize: 22))),
+                        )),
+                        const SizedBox(height: 32),
+                      ],
+                    ))
+              ])));
+        });
   }
 }
