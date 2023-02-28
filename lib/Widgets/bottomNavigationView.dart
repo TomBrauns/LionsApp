c6ff3fb390bef7b4401d05f61bc8d50a4d75ed00
Omer Guimdo <inf3249@hs-worms.de>
2023-02-28 15:04:06 +0100

    Added tooltip text to bottomNavigationView Icons

--- lib/Widgets/bottomNavigationView.dart
+++ lib/Widgets/bottomNavigationView.dart
@@ -21,76 +21,94 @@ class _BottomNavigationState extends State<BottomNavigation> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            IconButton(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              onPressed: () {
-                // Update State of App
-                Navigator.pop(context);
-                // Push to Screen
+            Tooltip(
+                message: "User Verwaltung",
+                decoration: BoxDecoration(color: Colors.blue),
+                child: IconButton(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  onPressed: () {
+                    // Update State of App
+                    Navigator.pop(context);
+                    // Push to Screen
 
-                Navigator.pushNamed(context, '/User');
-              },
-              icon: const Icon(Icons.badge, color: Colors.white),
-            ),
-            IconButton(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              onPressed: () {
-                // Update State of App
-                Navigator.pop(context);
-                // Push to Screen
+                    Navigator.pushNamed(context, '/User');
+                  },
+                  icon: const Icon(Icons.badge),
+                )),
+            Tooltip(
+                message: "Spenden",
+                decoration: BoxDecoration(color: Colors.blue),
+                child: IconButton(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  onPressed: () {
+                    // Update State of App
+                    Navigator.pop(context);
+                    // Push to Screen
 
-                Navigator.pushNamed(context, '/Donations');
-              },
-              icon: const Icon(Icons.card_giftcard, color: Colors.white),
-            ),
+                    Navigator.pushNamed(context, '/Donations');
+                  },
+                  icon: const Icon(Icons.card_giftcard),
+                )),
             if (Privileges.privilege == "Admin" ||
                 Privileges.privilege == "Member")
-              IconButton(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                onPressed: () {
-                  // Update State of App
-                  Navigator.pop(context);
-                  // Push to Screen
+              Tooltip(
+                  message: "Chat",
+                  decoration: BoxDecoration(color: Colors.blue),
+                  child: IconButton(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    onPressed: () {
+                      // Update State of App
+                      Navigator.pop(context);
+                      // Push to Screen
 
-                  Navigator.pushNamed(context, '/Chat');
-                },
-                icon: const Icon(Icons.chat, color: Colors.white),
-              ),
+                      Navigator.pushNamed(context, '/Chat');
+                    },
+                    icon: const Icon(Icons.chat),
+                  )),
             if (Privileges.privilege == "Admin" ||
                 Privileges.privilege == "Member")
-              IconButton(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                onPressed: () {
-                  // Update State of App
-                  Navigator.pop(context);
-                  // Push to Screen
+              Tooltip(
+                  message: "Kalender",
+                  decoration: BoxDecoration(color: Colors.blue),
+                  child: IconButton(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    onPressed: () {
+                      // Update State of App
+                      Navigator.pop(context);
+                      // Push to Screen
 
-                  Navigator.pushNamed(context, '/Calendar');
-                },
-                icon: const Icon(Icons.calendar_month_rounded, color: Colors.white),
-              ),
-            IconButton(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              onPressed: () {
-                // Update State of App
-                Navigator.pop(context);
-                // Push to Screen
+                      Navigator.pushNamed(context, '/Calendar');
+                    },
+                    icon: const Icon(Icons.calendar_month_rounded),
+                  )),
+            Tooltip(
+                message: "Katalog",
+                decoration: BoxDecoration(color: Colors.blue),
+                child: IconButton(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  onPressed: () {
+                    // Update State of App
+                    Navigator.pop(context);
+                    // Push to Screen
 
-                Navigator.pushNamed(context, '/Catalogue');
-              },
-              icon: const Icon(Icons.book, color: Colors.white),
-            ),
-            IconButton(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              onPressed: () {
-                // Update State of App
-                Navigator.pop(context);
-                // Push to Screen
+                    Navigator.pushNamed(context, '/Catalogue');
+                  },
+                  icon: const Icon(Icons.book),
+                )),
+            Tooltip(
+                message: "Aktivitäten",
+                decoration: BoxDecoration(color: Colors.blue),
+                child: IconButton(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  onPressed: () {
+                    // Update State of App
+                    Navigator.pop(context);
+                    // Push to Screen
 
-                Navigator.pushNamed(context, '/Events');
-              },
-              icon: const Icon(Icons.event, color: Colors.white),
-            ),
+                    Navigator.pushNamed(context, '/Events');
+                  },
+                  icon: const Icon(Icons.event),
+                )),
           ],
         ));
   }
