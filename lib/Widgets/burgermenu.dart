fd420d314e3533533a40f6646ffd260c0d399e1f
Marc Wieland <inf3457@hs-worms.de>
2023-03-01 15:08:11 +0100

    List Tiles können überall angeklickt werden -> Event Liste

--- lib/Widgets/burgermenu.dart
+++ lib/Widgets/burgermenu.dart
@@ -34,7 +34,6 @@ class _BurgerMenuState extends State<BurgerMenu> {
     return index == _selectedMenuIndex;
   }
 
-
   @override
   Widget build(BuildContext context) {
     print("Hier kommt die AppDat ${AppData.selected}");
