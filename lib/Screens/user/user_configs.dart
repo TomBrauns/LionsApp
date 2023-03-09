f20d3fac6300fcea3ff36162e7581a41cb6ca377
Mikulas Willaschek <willaschekmikulas@gmail.com>
2023-03-09 15:53:46 +0100

    fix notification problem

--- lib/Screens/user/user_configs.dart
+++ lib/Screens/user/user_configs.dart
@@ -27,15 +27,38 @@ class User extends StatefulWidget {
 
 class _UserState extends State<User> {
   bool _permissionGranted = false;
-
+  bool receiveNotifications = true;
   Future<void> _checkPermission() async {
     final status = await Permission.photos.request();
-    setState(() {
-      _permissionGranted = status == PermissionStatus.granted;
-    });
+    setState(
+      () {
+        _permissionGranted = status == PermissionStatus.granted;
+      },
+    );
   }
 
   @override
+  void initState() {
+    super.initState();
+    getReceiveNotifications();
+    _loadReceiveNotifications();
+  }
+
+  Future<void> _loadReceiveNotifications() async {
+    bool value = await getReceiveNotifications();
+    setState(
+      () {
+        receiveNotifications = value;
+      },
+    );
+  }
+
+  Future<bool> getReceiveNotifications() async {
+    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).get();
+    bool receiveNotifications = await userSnapshot.get('receiveNotification');
+    return receiveNotifications;
+  }
+
   bool test = true;
 
   Widget build(BuildContext context) {
@@ -72,20 +95,11 @@ class _UserState extends State<User> {
                       if (user != null) {
                         final XFile? file = await ImageUpload.selectImage();
                         if (file != null) {
-                          final String uniqueFilename =
-                              DateTime.now().millisecondsSinceEpoch.toString();
-                          final String? imageUrl =
-                              await ImageUpload.uploadImage(file, "user_images",
-                                  user.uid, uniqueFilename);
+                          final String uniqueFilename = DateTime.now().millisecondsSinceEpoch.toString();
+                          final String? imageUrl = await ImageUpload.uploadImage(file, "user_images", user.uid, uniqueFilename);
                           if (imageUrl != null) {
-                            await FirebaseFirestore.instance
-                                .collection('users')
-                                .doc(user.uid)
-                                .update({"image_url": imageUrl});
-                            await FirebaseFirestore.instance
-                                .collection('user_chat')
-                                .doc(user.uid)
-                                .update({"imageUrl": imageUrl});
+                            await FirebaseFirestore.instance.collection('users').doc(user.uid).update({"image_url": imageUrl});
+                            await FirebaseFirestore.instance.collection('user_chat').doc(user.uid).update({"imageUrl": imageUrl});
                           }
                         }
                       } else {
@@ -100,8 +114,7 @@ class _UserState extends State<User> {
                         );
                       }
                     },
-                    child:
-                        Text('Profilbild ändern', style: CustomTextSize.small),
+                    child: Text('Profilbild ändern', style: CustomTextSize.small),
                   ),
                   if (user != null)
                     UserDataWidget()
@@ -111,17 +124,14 @@ class _UserState extends State<User> {
                       style: TextStyle(color: Colors.red),
                     ),
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    Text('App Benachrichtigungen:',
-                        style: CustomTextSize.small),
+                    Text('App Benachrichtigungen:', style: CustomTextSize.small),
                     Switch.adaptive(
-                      value: test,
+                      value: receiveNotifications,
                       onChanged: (newValue) {
                         setState(
                           () {
-                            test = newValue;
-                            final docRef = FirebaseFirestore.instance
-                                .collection('users')
-                                .doc(FirebaseAuth.instance.currentUser!.uid);
+                            receiveNotifications = newValue;
+                            final docRef = FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid);
                             docRef.update(
                               {
                                 'receiveNotification': newValue,
@@ -158,8 +168,7 @@ class _UserState extends State<User> {
                     Icons.badge,
                     size: 24.0,
                   ),
-                  label:
-                      Text('Nutzerdaten ändern', style: CustomTextSize.medium),
+                  label: Text('Nutzerdaten ändern', style: CustomTextSize.medium),
                   style: ElevatedButton.styleFrom(
                     elevation: 0,
                   ),
@@ -279,8 +288,7 @@ class _UserState extends State<User> {
                     );
                     Navigator.push(
                       context,
-                      MaterialPageRoute(
-                          builder: (context) => const Donations()),
+                      MaterialPageRoute(builder: (context) => const Donations()),
                     );
                   },
                 ),
@@ -321,10 +329,8 @@ class _UserState extends State<User> {
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
-                Text('Wollen Sie Ihren Account wirklich löschen?',
-                    style: CustomTextSize.small),
-                Text('Der Vorgang kann nicht rückgängig gemacht werden',
-                    style: CustomTextSize.small),
+                Text('Wollen Sie Ihren Account wirklich löschen?', style: CustomTextSize.small),
+                Text('Der Vorgang kann nicht rückgängig gemacht werden', style: CustomTextSize.small),
               ],
             ),
           ),
@@ -367,12 +373,8 @@ class _UserState extends State<User> {
     try {
       final user = FirebaseAuth.instance.currentUser!;
       return StreamBuilder<DocumentSnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('users')
-            .doc(user.uid)
-            .snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        stream: FirebaseFirestore.instance.collection('users').doc(user.uid).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const CircularProgressIndicator();
           } else if (snapshot.hasError) {
@@ -406,10 +408,7 @@ class _UserState extends State<User> {
 
 Future<void> deleteAcc() async {
   Privileges.privilege = Privilege.guest;
-  await FirebaseFirestore.instance
-      .collection('users')
-      .doc(FirebaseAuth.instance.currentUser!.uid)
-      .delete();
+  await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).delete();
   await FirebaseAuth.instance.currentUser!.delete();
 }
 
@@ -436,13 +435,7 @@ class _SubsState extends State<Subs> {
   }
 }
 
-const List<String> list = <String>[
-  'keins',
-  'Protanopie',
-  'Deuteranopie',
-  'Tritanopie',
-  'Achromatopsie'
-];
+const List<String> list = <String>['keins', 'Protanopie', 'Deuteranopie', 'Tritanopie', 'Achromatopsie'];
 
 class Accessibility extends StatefulWidget {
   const Accessibility({super.key});
@@ -547,32 +540,17 @@ class UserDataWidget extends StatelessWidget {
       return const Text('gerade niemand eingeloggt');
     } else {
       return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        future:
-            FirebaseFirestore.instance.collection('users').doc(userId).get(),
+        future: FirebaseFirestore.instance.collection('users').doc(userId).get(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
             final userData = snapshot.data!.data()!;
 
             return Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                if (userData['firstname'] != null &&
-                    userData['lastname'] != null)
-                  Text('Name: ${userData['firstname']} ${userData['lastname']}',
-                      style: CustomTextSize.small),
-                if (userData['email'] != null)
-                  Text('Email: ${userData['email']}',
-                      style: CustomTextSize.small),
-                if (userData['streetname'] != null &&
-                    userData['streetnumber'] != null &&
-                    userData['postalcode'] != null &&
-                    userData['cityname'] != null)
-                  Text(
-                      'Addresse: ${userData['streetname']} ${userData['streetnumber']} ${userData['postalcode']} ${userData['cityname']}',
-                      style: CustomTextSize.small)
-                else
-                  (Text('')),
+                if (userData['firstname'] != null && userData['lastname'] != null) Text('Name: ${userData['firstname']} ${userData['lastname']}', style: CustomTextSize.small),
+                if (userData['email'] != null) Text('Email: ${userData['email']}', style: CustomTextSize.small),
+                if (userData['streetname'] != null && userData['streetnumber'] != null && userData['postalcode'] != null && userData['cityname'] != null) Text('Addresse: ${userData['streetname']} ${userData['streetnumber']} ${userData['postalcode']} ${userData['cityname']}', style: CustomTextSize.small) else (Text('')),
               ],
             );
           } else if (snapshot.hasError) {
