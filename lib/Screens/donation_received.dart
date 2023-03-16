731ecf6faf5214e522d2ca7c431bd8d8525d1ca3
Philipp Muders <philipp.muders@googlemail.com>
2023-03-16 13:29:31 +0100

    we shouldnt pop if we dont have to

--- lib/Screens/donation_received.dart
+++ lib/Screens/donation_received.dart
@@ -13,7 +13,8 @@ import '../../Widgets/appbar.dart';
 import 'package:lionsapp/Widgets/burgermenu.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_social_button/flutter_social_button.dart';
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:open_filex/open_filex.dart';
 import 'dart:io';
@@ -47,28 +48,33 @@ class DonationReceived extends StatefulWidget {
 
 class _DonationReceivedState extends State<DonationReceived> {
   String get Id {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     return args?['Id'];
   }
 
   double get amount {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     return args?['amount'];
   }
 
   String get sub {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     return args?['sub'];
   }
 
   String get Idtype {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     return args?['Idtype'];
   }
 
   Future<void> sendMailWithReceipt(String eventName, String pdf) async {
     final userId = FirebaseAuth.instance.currentUser!.uid;
-    final docSnapshot = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    final docSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
     final userData = docSnapshot.data() as Map<String, dynamic>;
     String firstName = userData['firstname'] as String;
     String lastName = userData['lastname'] as String;
@@ -79,11 +85,12 @@ class _DonationReceivedState extends State<DonationReceived> {
         'from': 'Team Lions',
         'to': eMail,
         'subject': 'Danke für Ihre Spende! Ihre Spendenquittung',
-        'text': 'Hallo $firstName $lastName \nWir bedanken uns recht herzlich für Ihre Spende an $eventName in Höhe von $amount!.\n'
-            'Damit tust etwas gutes usw.\n'
-            'Im Anhang finden Sie Ihre Spendenquittung!\n'
-            'Mit freundlichen Grüßen\n'
-            'Deine Lions Team\n',
+        'text':
+            'Hallo $firstName $lastName \nWir bedanken uns recht herzlich für Ihre Spende an $eventName in Höhe von $amount!.\n'
+                'Damit tust etwas gutes usw.\n'
+                'Im Anhang finden Sie Ihre Spendenquittung!\n'
+                'Mit freundlichen Grüßen\n'
+                'Deine Lions Team\n',
         'attachments': [
           //TODO: Pfad der späteren Quittung und Text bisschen anpassen
           {'filename': 'Spenden_Quittung.pdf', 'path': pdf},
@@ -91,7 +98,8 @@ class _DonationReceivedState extends State<DonationReceived> {
       }
     };
 
-    final HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('sendEmailWithAttachments');
+    final HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('sendEmailWithAttachments');
     try {
       await callable(data);
     } catch (e) {
@@ -100,13 +108,16 @@ class _DonationReceivedState extends State<DonationReceived> {
   }
 
   Future<void> _updateEventDonation(String eventId) async {
-    final documentRef = FirebaseFirestore.instance.collection('events').doc(eventId);
+    final documentRef =
+        FirebaseFirestore.instance.collection('events').doc(eventId);
     final event = await documentRef.get();
     final double currentValue = event.get("currentDonationValue");
-    return await documentRef.update({'currentDonationValue': currentValue + amount});
+    return await documentRef
+        .update({'currentDonationValue': currentValue + amount});
   }
 
-  Future<void> _updateDonationHistory(String id, String name, String type, String url) async {
+  Future<void> _updateDonationHistory(
+      String id, String name, String type, String url) async {
     final User? currentUser = FirebaseAuth.instance.currentUser;
     await FirebaseFirestore.instance.collection("donations").add(
       {
@@ -140,7 +151,7 @@ class _DonationReceivedState extends State<DonationReceived> {
             if (!snapshot.hasData || !snapshot.data!.exists) {
               return const Center(child: Text('Event nicht gefunden'));
             } else {
-              if(Idtype == "events") {
+              if (Idtype == "events") {
                 name = snapshot.data!.get("eventName");
               } else {
                 name = snapshot.data!.get("name");
@@ -150,7 +161,8 @@ class _DonationReceivedState extends State<DonationReceived> {
             name = "Dort wo es benötigt wird.";
           }
 
-          final message = "Danke für Ihre Spende von $amount€ an $name. Wenn Sie uns noch etwas mitteilen möchten, zögern Sie nicht, uns über das Kontaktformular zu benachrichtigen.";
+          final message =
+              "Danke für Ihre Spende von $amount€ an $name. Wenn Sie uns noch etwas mitteilen möchten, zögern Sie nicht, uns über das Kontaktformular zu benachrichtigen.";
 
           void makeSurePdfUploadGetsExecutedJustOnce() {
             if (isHandled) {
@@ -158,13 +170,14 @@ class _DonationReceivedState extends State<DonationReceived> {
             } else {
               _ReceiptState()._handlePdfUpload().then(
                 (pdfUrl) {
-                  if (FirebaseAuth.instance.currentUser != null && pdfUrl != null) {
+                  if (FirebaseAuth.instance.currentUser != null &&
+                      pdfUrl != null) {
                     sendMailWithReceipt(name, pdfUrl);
                   }
                   if (Idtype == "events") {
                     _updateEventDonation(Id);
                   }
-                  if(sub == "keins") {
+                  if (sub == "keins") {
                     _updateDonationHistory(Id, name, Idtype, pdfUrl ?? "");
                   }
                 },
@@ -207,9 +220,9 @@ class _DonationReceivedState extends State<DonationReceived> {
                       ),
                       SizedBox(height: 10),
                       ElevatedButton(
-                        child: Text('zur Zuwendungsbestätigung', style: CustomTextSize.medium),
+                        child: Text('zur Zuwendungsbestätigung',
+                            style: CustomTextSize.medium),
                         onPressed: () {
-                          Navigator.pop(context);
                           Navigator.pushNamed(context, '/ThankYou/Receipt');
                         },
                         style: ElevatedButton.styleFrom(
@@ -284,7 +297,8 @@ class _DonationReceivedState extends State<DonationReceived> {
                       )),
                   onPressed: () {
                     print("Rufe Share Button mit eventId auf: $Id");
-                    Navigator.pushNamed(context, '/ThankYou/ShareDonation', arguments: {'eventId': Id});
+                    Navigator.pushNamed(context, '/ThankYou/ShareDonation',
+                        arguments: {'eventId': Id});
                   },
                 ),
               ),
@@ -292,7 +306,8 @@ class _DonationReceivedState extends State<DonationReceived> {
                 padding: const EdgeInsets.all(20.0),
                 width: 400,
                 child: ElevatedButton(
-                  child: Text('Zurück zum Spenden', style: CustomTextSize.medium),
+                  child:
+                      Text('Zurück zum Spenden', style: CustomTextSize.medium),
                   onPressed: () {
                     // Push to Screen
                     Navigator.pushNamed(context, '/Donations');
@@ -403,7 +418,9 @@ class _ReceiptState extends State<Receipt> {
             Container(
               margin: const EdgeInsets.all(40),
               height: 50,
-              child: Text("Als angemeldeter Nutzer erhalten Sie automatisch eine Quittung per Mail", style: CustomTextSize.small),
+              child: Text(
+                  "Als angemeldeter Nutzer erhalten Sie automatisch eine Quittung per Mail",
+                  style: CustomTextSize.small),
             ),
           ],
         ),
@@ -415,8 +432,12 @@ class _ReceiptState extends State<Receipt> {
     PdfDocument document = PdfDocument();
     final page = document.pages.add();
 
-    page.graphics.drawImage(PdfBitmap(await _readImageData()), Rect.fromLTWH(0, 0, page.getClientSize().width, page.getClientSize().height));
-    page.graphics.drawString("Test", PdfStandardFont(PdfFontFamily.helvetica, 10));
+    page.graphics.drawImage(
+        PdfBitmap(await _readImageData()),
+        Rect.fromLTWH(
+            0, 0, page.getClientSize().width, page.getClientSize().height));
+    page.graphics
+        .drawString("Test", PdfStandardFont(PdfFontFamily.helvetica, 10));
 
     List<int> bytes = await document.save();
     document.dispose();
@@ -427,7 +448,10 @@ class _ReceiptState extends State<Receipt> {
   Future<String?> _handlePdfUpload() async {
     if (kIsWeb) {
       final uniqueId = "Quittung_${DateTime.now().millisecondsSinceEpoch}.pdf";
-      final reference = FirebaseStorage.instance.ref('donator_receipts').child(FirebaseAuth.instance.currentUser!.uid).child(uniqueId);
+      final reference = FirebaseStorage.instance
+          .ref('donator_receipts')
+          .child(FirebaseAuth.instance.currentUser!.uid)
+          .child(uniqueId);
       //Web
 
       final bytes = await _createPDF();
@@ -441,10 +465,14 @@ class _ReceiptState extends State<Receipt> {
       final bytes = await _createPDF();
 
       final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/Quittung_${DateTime.now().millisecondsSinceEpoch}.pdf');
+      final file = File(
+          '${directory.path}/Quittung_${DateTime.now().millisecondsSinceEpoch}.pdf');
       await file.writeAsBytes(bytes);
 
-      final reference = FirebaseStorage.instance.ref('donator_receipts').child(FirebaseAuth.instance.currentUser!.uid).child(file.path);
+      final reference = FirebaseStorage.instance
+          .ref('donator_receipts')
+          .child(FirebaseAuth.instance.currentUser!.uid)
+          .child(file.path);
       await reference.putFile(file);
 
       return reference.getDownloadURL();
@@ -471,7 +499,9 @@ class _ReceiptState extends State<Receipt> {
   void _handleWebDownloadButtonPressed() async {
     List<int> bytes = await _createPDF();
 
-    html.AnchorElement(href: "data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}")
+    html.AnchorElement(
+        href:
+            "data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}")
       ..setAttribute("download", "spendenquittung.pdf")
       ..click();
   }
@@ -486,14 +516,18 @@ class ShareDonation extends StatefulWidget {
 
 Future<void> shareToFacebook(String url) async {
   if (GetPlatform.currentPlatform == GetPlatform.web) {
-    if (await canLaunchUrl(Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"))) {
-      await launchUrl(Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"));
+    if (await canLaunchUrl(
+        Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"))) {
+      await launchUrl(
+          Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"));
     } else {
       //print("Could not launch URL");
     }
   } else {
-    if (await canLaunchUrl(Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"))) {
-      await launchUrl(Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"));
+    if (await canLaunchUrl(
+        Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"))) {
+      await launchUrl(
+          Uri.parse("https://www.facebook.com/sharer/sharer.php?u=$url"));
     } else {
       //print("Could not launch URL");
     }
@@ -510,7 +544,8 @@ Future<void> shareToTwitter(String url) async {
       //print("Could not launch URL");
     }
   } else {
-    if (await canLaunchUrl(Uri.parse("https://twitter.com/intent/tweet?url=$url"))) {
+    if (await canLaunchUrl(
+        Uri.parse("https://twitter.com/intent/tweet?url=$url"))) {
       await launchUrl(Uri.parse("https://twitter.com/intent/tweet?url=$url"));
     } else {
       //print("Could not launch URL");
@@ -520,7 +555,8 @@ Future<void> shareToTwitter(String url) async {
 
 class _ShareDonationState extends State<ShareDonation> {
   String? get eventId {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     return args?['eventId'];
   }
 
@@ -541,7 +577,8 @@ class _ShareDonationState extends State<ShareDonation> {
               FlutterSocialButton(
                 onTap: () async {
                   try {
-                    await shareToFacebook('https://serviceclub-app.de/#/Donations?eventId=$eventId');
+                    await shareToFacebook(
+                        'https://serviceclub-app.de/#/Donations?eventId=$eventId');
                   } catch (e) {
                     //print("Failed to share to Facebook: $e");
                   }
@@ -558,7 +595,8 @@ class _ShareDonationState extends State<ShareDonation> {
               FlutterSocialButton(
                 onTap: () async {
                   try {
-                    await shareToTwitter('https://serviceclub-app.de/#/Donations?eventId=$eventId');
+                    await shareToTwitter(
+                        'https://serviceclub-app.de/#/Donations?eventId=$eventId');
                   } catch (e) {
                     //print("Failed to share to Twitter: $e");
                   }
