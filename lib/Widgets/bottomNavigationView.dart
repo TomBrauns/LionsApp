090fb79a2ea449df562ddc047088383f6e4fbfd1
Philipp Muders <philipp.muders@googlemail.com>
2023-03-16 11:02:05 +0100

    bottomnav conditionally

--- lib/Widgets/bottomNavigationView.dart
+++ lib/Widgets/bottomNavigationView.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:lionsapp/Widgets/privileges.dart';
 import 'package:lionsapp/util/color.dart';
 import 'package:lionsapp/Widgets/burgermenu.dart';
+import 'package:lionsapp/Widgets/privileges.dart';
 
 class BottomNavigation extends StatefulWidget {
   @override
@@ -12,108 +13,113 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-        color: ColorUtils.primaryColor,
-        shape: const CircularNotchedRectangle(), //shape of notch
-        notchMargin: 3,
-        elevation: 0,
-        height: 80,
-        clipBehavior: Clip.none,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Tooltip(
-                message: "User Verwaltung",
-                decoration: const BoxDecoration(color: ColorUtils.primaryColor),
-                child: IconButton(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  onPressed: () {
-                    // Update State of App
-                    Navigator.pop(context);
-                    // Push to Screen
-                    AppData.selected = 2;
+    return Privileges.privilege != Privilege.guest
+        ? BottomAppBar(
+            color: ColorUtils.primaryColor,
+            shape: const CircularNotchedRectangle(), //shape of notch
+            notchMargin: 3,
+            elevation: 0,
+            height: 80,
+            clipBehavior: Clip.none,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Tooltip(
+                    message: "User Verwaltung",
+                    decoration:
+                        const BoxDecoration(color: ColorUtils.primaryColor),
+                    child: IconButton(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      onPressed: () {
+                        // Update State of App
+                        Navigator.pop(context);
+                        // Push to Screen
+                        AppData.selected = 2;
 
-                    Navigator.pushNamed(context, '/User');
-                  },
-                  icon: const Icon(Icons.badge, color: Colors.white),
-                ),
-              ),
-              if (Privileges.privilege == Privilege.admin ||
-                  Privileges.privilege == Privilege.member)
-                Tooltip(
-                  message: "Chat",
-                  decoration:
-                      const BoxDecoration(color: ColorUtils.primaryColor),
-                  child: IconButton(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    onPressed: () {
-                      // Update State of App
-                      Navigator.pop(context);
-                      // Push to Screen
-                      AppData.selected = 6;
-
-                      Navigator.pushNamed(context, '/Chat');
-                    },
-                    icon: const Icon(Icons.chat, color: Colors.white),
+                        Navigator.pushNamed(context, '/User');
+                      },
+                      icon: const Icon(Icons.badge, color: Colors.white),
+                    ),
                   ),
-                ),
-              if (Privileges.privilege == Privilege.admin ||
-                  Privileges.privilege == Privilege.member)
-                Tooltip(
-                  message: "Kalender",
-                  decoration:
-                      const BoxDecoration(color: ColorUtils.primaryColor),
-                  child: IconButton(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    onPressed: () {
-                      // Update State of App
-                      Navigator.pop(context);
-                      // Push to Screen
-                      AppData.selected = 3;
+                  if (Privileges.privilege == Privilege.admin ||
+                      Privileges.privilege == Privilege.member)
+                    Tooltip(
+                      message: "Chat",
+                      decoration:
+                          const BoxDecoration(color: ColorUtils.primaryColor),
+                      child: IconButton(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        onPressed: () {
+                          // Update State of App
+                          Navigator.pop(context);
+                          // Push to Screen
+                          AppData.selected = 6;
 
-                      Navigator.pushNamed(context, '/Calendar');
-                    },
-                    icon: const Icon(Icons.calendar_month_rounded,
-                        color: Colors.white),
+                          Navigator.pushNamed(context, '/Chat');
+                        },
+                        icon: const Icon(Icons.chat, color: Colors.white),
+                      ),
+                    ),
+                  if (Privileges.privilege == Privilege.admin ||
+                      Privileges.privilege == Privilege.member)
+                    Tooltip(
+                      message: "Kalender",
+                      decoration:
+                          const BoxDecoration(color: ColorUtils.primaryColor),
+                      child: IconButton(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        onPressed: () {
+                          // Update State of App
+                          Navigator.pop(context);
+                          // Push to Screen
+                          AppData.selected = 3;
+
+                          Navigator.pushNamed(context, '/Calendar');
+                        },
+                        icon: const Icon(Icons.calendar_month_rounded,
+                            color: Colors.white),
+                      ),
+                    ),
+                  Tooltip(
+                    message: "Projekte",
+                    decoration:
+                        const BoxDecoration(color: ColorUtils.primaryColor),
+                    child: IconButton(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      onPressed: () {
+                        // Update State of App
+                        Navigator.pop(context);
+                        // Push to Screen
+                        AppData.selected = 5;
+
+                        Navigator.pushNamed(context, '/Catalogue');
+                      },
+                      icon: const Icon(Icons.book, color: Colors.white),
+                    ),
                   ),
-                ),
-              Tooltip(
-                message: "Projekte",
-                decoration: const BoxDecoration(color: ColorUtils.primaryColor),
-                child: IconButton(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  onPressed: () {
-                    // Update State of App
-                    Navigator.pop(context);
-                    // Push to Screen
-                    AppData.selected = 5;
+                  Tooltip(
+                    message: "Aktivitäten",
+                    decoration:
+                        const BoxDecoration(color: ColorUtils.primaryColor),
+                    child: IconButton(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      onPressed: () {
+                        // Update State of App
+                        Navigator.pop(context);
+                        // Push to Screen
+                        AppData.selected = 4;
 
-                    Navigator.pushNamed(context, '/Catalogue');
-                  },
-                  icon: const Icon(Icons.book, color: Colors.white),
-                ),
+                        Navigator.pushNamed(context, '/Events');
+                      },
+                      icon: const Icon(Icons.event, color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
-              Tooltip(
-                message: "Aktivitäten",
-                decoration: const BoxDecoration(color: ColorUtils.primaryColor),
-                child: IconButton(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  onPressed: () {
-                    // Update State of App
-                    Navigator.pop(context);
-                    // Push to Screen
-                    AppData.selected = 4;
-
-                    Navigator.pushNamed(context, '/Events');
-                  },
-                  icon: const Icon(Icons.event, color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ));
+            ))
+        : SizedBox.shrink();
   }
 }
